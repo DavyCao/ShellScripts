cc73421a14feed211b0f54f5f3b866dc2fc1ad0e
Yi Cao <yicao1@cs.stonybrook.edu>
2018-12-11 21:27:04 -0500

    Update increase_buffer.sh

--- increase_buffer.sh
+++ increase_buffer.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 sysctl -w net.ipv4.tcp_rmem='4096 87380 2147483647'
 sysctl -w net.ipv4.tcp_wmem='4096 65536 2147483647'
-sysctl -w net.ipv4.tcp_mem='85968 114627 171936'
 sysctl -w net.ipv4.tcp_no_metrics_save=1
 sysctl -w net.ipv4.route.flush=1
