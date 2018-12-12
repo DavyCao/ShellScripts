7b4f968e451f533b06deef687716e3489a2ffe22
Yi Cao <yicao1@cs.stonybrook.edu>
2018-12-12 01:49:24 -0500

    Rename ubuntu_deps.sh to ubuntu-deps

--- ubuntu_deps.sh
+++
@@ -1,3 +0,0 @@
-sudo apt-get update
-sudo apt-get install net-tools ethtool ssh iperf iperf3 git curl zsh vim screen
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
